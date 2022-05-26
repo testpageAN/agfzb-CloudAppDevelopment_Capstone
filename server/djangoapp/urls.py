5c8fe109c8c86db2631d8198ff842f9e63ca91fb
testpageAN <alexis.nikolopoulos@hotmail.com>
2022-05-26 20:38:45 +0000

    Adding temporary changes to Github 26/5-2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+     path(route='', view=views.get_dealerships, name='index'),
+    
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
+from .models import CarModel
 # from .restapis import related methods
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -91,6 +93,17 @@ def get_dealerships(request):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 # ...
+def get_dealerships(request):
+    if request.method == "GET":
+        url = "https://e845b9da.eu-gb.apigw.appdomain.cloud/api/dealership"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
+
+
 
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
