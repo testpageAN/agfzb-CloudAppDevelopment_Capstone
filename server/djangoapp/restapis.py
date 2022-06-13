1869505e4a4ef14e34aab081aed580cd8982ee47
testpageAN <alexis.nikolopoulos@hotmail.com>
2022-06-13 18:53:51 +0000

    Adding temporary changes to Github 13/06-2

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,7 +3,6 @@ import json
 import os
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
-from decouple import config
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
